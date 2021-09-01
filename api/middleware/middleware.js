7c6dc607e8bf793bb0de0fec8518d4d804eeaef9
andrewskr90 <andrewskr90@gmail.com>
2021-09-01 15:51:49 -0700

    half of tests passing

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,56 @@
+const Users = require('../users/users-model')
+
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  console.log(`Request Method: ${req.method}`)
+  console.log(`Request URL: ${req.url}`)
+  console.log(Date())
+  next()
 }
 
 function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+  const { id } = req.params
+  Users.getById(id)
+    .then(user => {
+      if (user) {
+        req.user = user
+        next()
+      } else {
+        next({ message: 'user not found', status: 404 })
+      }
+    })
+    .catch(err => {
+      next(err)
+    })
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+  const { name } = req.body
+  if (name.trim() !== '' && name) {
+    next()
+  } else {
+    next({
+      message: "missing required name field",
+      status: 400
+    })
+  }
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+  const { text } = req.body
+  if (text) {
+    next()
+  } else {
+    next({
+      message: "missing required text field",
+      status: 400
+    })
+  }
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost
+}
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,44 +1,80 @@
 const express = require('express');
 
 // You will need `users-model.js` and `posts-model.js` both
+const Users = require('./users-model')
+const Posts = require('../posts/posts-model')
 // The middleware functions also need to be required
+const { validateUserId, validateUser, validatePost } = require('../middleware/middleware')
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // RETURN AN ARRAY WITH ALL THE USERS
+  Users.get()
+    .then(users => {
+      if (users.length<1) {
+        res.status(500).json({
+          message: "failed to get users"
+        })
+      } else {
+        res.status(200).json(users)
+      }
+    })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateUserId, (req, res) => {
+  res.status(200).json(req.user)
   // RETURN THE USER OBJECT
   // this needs a middleware to verify user id
 });
 
-router.post('/', (req, res) => {
+router.post('/', validateUser, (req, res) => {
+  const { body } = req
+  Users.insert(body)
+    .then(newUser => {
+      res.status(201).json(newUser)
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: err.message
+      })
+    })
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validateUserId, validateUser, (req, res) => {
+  Users.update(req.user.id, req.body)
+    .then(updatedUser => {
+      res.status(200).json(updatedUser)
+    })
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validateUserId, (req, res) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', validateUserId, (req, res) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
 });
 
+router.use((err, req, res, next) => {
+  console.log(err.message)
+  res.status(err.status || 500).json({
+    message: err.message,
+    customMessage: "Failed to make it past the User router"
+  })
+})
+
 // do not forget to export the router
+module.exports = router
